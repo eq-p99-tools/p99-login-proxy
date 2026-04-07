9e2f531bde916fdf54d0e5ca763354497df57ff5
Adam Harwell <flux.adam@gmail.com>
2026-04-07 02:43:47 -0400

    Bump to rc2

--- p99_sso_login_proxy/__init__.py
+++ p99_sso_login_proxy/__init__.py
@@ -1,4 +1,4 @@
 import semver
 
-__version_semver__ = semver.Version(major=1, minor=3, patch=1, prerelease="rc1", build=None)
+__version_semver__ = semver.Version(major=1, minor=3, patch=1, prerelease="rc2", build=None)
 __version__ = str(__version_semver__)
